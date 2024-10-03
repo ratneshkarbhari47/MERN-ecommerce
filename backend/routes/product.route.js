e21850f32a726094d92ec431ccc79478672b9507
ratneshkarbhari47 <ratneshkarbhari10@gmail.com>
2024-10-03 10:14:14 +0530

    using a controller for Products api and it works

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,86 +1,21 @@
 import express from "express"
 import Product from "../models/Product.model.js"
+import { createProduct, deleteProduct, getProducts ,updateProduct } from "../controllers/Product.controller.js"
 
 const router = express.Router()
 
 
 // Product routes
-router.get("/",async(req,res)=>{
-    try {
-        const products = await Product.find({})
-        res.status(200).json({
-            "success" : true,
-            "data" : products
-        })
-    } catch (error) {
-        console.log("Error in fetching products : "+error)
-        res.status(500).json({
-            "success" : false,
-            "message" : "No products found"
-        })
-    }
-})
+router.get("/",getProducts)
 
-router.post("/", async(req,res)=>{
-    const product = req.body
-    if(!product.name||!product.price||!product.image){
-        return res.status(400).json({
-            "success" : false,
-            "message" : "Please send all required params"
-        })
-    }
-    const newProduct = new Product(product);
+router.post("/", createProduct)
 
-    try {
-        await newProduct.save()
-        res.status(201).json({
-            "success" : true,
-            "message" : "Product created successfuly"
-        })
-    } catch (error) {
-        console.log("Error in create product "+error)
-        res.status(500).json(
-            {
-                "success" : false,
-                "message" : "Server error"+error
-            }
-        )
-    }
+router.put("/:id", updateProduct)
 
-})
+router.delete("/:id", deleteProduct)
 
-router.put("/:id", async(req,res)=>{
-    const {id} = req.params
-    const product = req.body
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(id,product,{new:true})
-        res.status(200).json({
-            "success" : true,
-            "data" : updatedProduct
-        })
-    } catch (error) {
-        res.status(500).json({
-            "success" : false,
-            "message" : "Server error : "+error
-        })
-    }
-})
 
-router.delete("/:id", async(req,res)=>{
-    const {id} =  req.params
-    try {
-        await Product.findByIdAndDelete(id)
-        res.status(200).json({
-            "success": true,
-            "message" : "Product deleted"
-        })
-    } catch (error) {
-        res.status(404).json({
-            "success": false,
-            "message" : "Product not found"
-        })
-    }
-})
+
 
 
 export default router
